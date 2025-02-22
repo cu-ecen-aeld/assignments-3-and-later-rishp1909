6c23cd71ab8cdb14939849b12ee07fd7403d15fd
rishp <rishp@Ubuntu.myguest.virtualbox.org>
2025-02-22 22:27:02 +0530

    [Assignment3-Part-2] completed

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,13 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,11 +52,16 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir "${OUTDIR}/rootfs"
+cd "${OUTDIR}/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
@@ -61,20 +70,37 @@ else
 fi
 
 # TODO: Make and install busybox
-
+make distclean
+make defconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- 
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- install
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+aarch64-none-linux-gnu-readelf -a "${OUTDIR}/busybox/busybox" | grep "program interpreter"
+aarch64-none-linux-gnu-readelf -a "${OUTDIR}/busybox/busybox" | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-
+cp /home/rishp/Softwares/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+cp /home/rishp/Softwares/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp /home/rishp/Softwares/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp /home/rishp/Softwares/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 # TODO: Make device nodes
 
 # TODO: Clean and build the writer utility
+cd /home/rishp/aesdback/finder-app
+make clean
+make CROSS_COMPILE=aarch64-none-linux-gnu-
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
+cp /home/rishp/assignment3/finder-app/host/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp /home/rishp/assignment3/finder-app/host/finder-test.sh ${OUTDIR}/rootfs/home/
+cp /home/rishp/assignment3/finder-app/host/finder.sh ${OUTDIR}/rootfs/home/
+cp /home/rishp/assignment3/finder-app/host/writer ${OUTDIR}/rootfs/home/
+cp -r /home/rishp/assignment3/finder-app/host/conf ${OUTDIR}/rootfs/home/
 # TODO: Chown the root directory
-
+cd "$OUTDIR/rootfs"
+sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
